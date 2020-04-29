d6fa46c4f70e658747b2790334212c05efd26f1a
xuyuefeng@192.168.0.119:29418 <2670476383@qq.com>
2020-04-29 13:34:37 +0800

    add scrollPlayerIntoView

--- draw/DOMDisplay.js
+++ draw/DOMDisplay.js
@@ -42,3 +42,27 @@ DOMDisplay.prototype.syncState = function(state) {
 	this.dom.className = `game ${state.status}`;
 	this.scrollPlayerIntoView(state);
 };
+DOMDisplay.prototype.scrollPlayerIntoView = function(state) {
+	let width = this.dom.clientWidth;
+	let height = this.dom.clientHeight;
+	let margin = width / 3;
+
+	let left = this.dom.scrollLeft, right = left + width;
+	let top = this.dom.scrollTop, bottom = top + height;
+
+	let player = state.player;
+	let center = player.pos
+		.plus(player.size.times(0.5))
+		.times(scale);
+
+	if (center.x < left + margin) {
+		this.dom.scrollLeft = center.x - margin;
+	} else if (center.x > right - margin) {
+		this.dom.scrollLeft = center.x + margin - width;
+	}
+	if (center.y < top + margin) {
+		this.dom.scrollTop = center.y - margin;
+	} else if (center.y > bottom - margin) {
+		this.dom.scrollTop = center.y + margin - height;
+	}
+};
