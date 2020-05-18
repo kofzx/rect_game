cf265861ec1ef4209549757b78755291f22f0d10
xuyuefeng@192.168.0.119:29418 <2670476383@qq.com>
2020-05-18 22:44:35 +0800

    implement monster & update readme

--- actors/Monster.js
+++ actors/Monster.js
@@ -1,4 +1,5 @@
 import Vec from '../utils/Vec.js'
+import State from '../main/State.js'
 
 class Monster {
 	constructor(pos, speed) {
@@ -9,15 +10,22 @@ class Monster {
 		return 'monster';
 	}
 	static create(pos) {
-		return new Monster(pos.plus(new Vec(0, -1)));
+		return new Monster(pos, new Vec(2, 0));
 	}
 }
-Monster.prototype.update = function(time) {
-
+Monster.prototype.update = function(time, state) {
+	let newPos = this.pos.plus(this.speed.times(time));
+	if (!state.level.touches(newPos, this.size, 'wall')) {
+		return new Monster(newPos, this.speed);
+	}
+	// 碰到了墙
+	else {
+		return new Monster(this.pos, this.speed.times(-1));
+	}
 }
 Monster.prototype.collide = function(state) {
-
+	return new State(state.level, state.actors, 'lost');
 }
-Monster.prototype.size = new Vec(1.2, 2);
+Monster.prototype.size = new Vec(1.2, 1);
 
 export default Monster;
